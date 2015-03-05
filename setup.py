0e7be451a8fa8b236232a1c85a782d8d1206053e
Massimo Menichinelli <info@openp2pdesign.org>
2015-03-05 16:56:02 +0100

    Add first version of setup files

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+setup(
+  name = 'makerspaces',
+  packages = ['mackerspaces'],
+  version = '0.1',
+  description = 'Python library for accessing online data about Makerspaces, Fab Labs, Hackerspaces, TechShop...',
+  author = 'Massimo Menichinelli',
+  author_email = 'info@openp2pdesign.org',
+  url = 'https://github.com/openp2pdesign/PyMakerspaces',
+  download_url = '',
+  keywords = ['FabLab', 'Makerspace', 'Hackerspace'],
+  classifiers = ["Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",],
+)
