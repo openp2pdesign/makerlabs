6fc7d7f19165bb2916636f4e88d5a6affadcc5fe
Massimo Menichinelli <info@openp2pdesign.org>
2015-12-15 22:35:38 +0100

    Various updates in style, requirements, hackerspaces API url

--- makerspaces/hackerspaces_org.py
+++ makerspaces/hackerspaces_org.py
@@ -9,216 +9,229 @@
 #
 
 from simplemediawiki import MediaWiki
-#Requirement: kitchen
 import mwparserfromhell
 
-hackerspaces_org_api_url = "http://hackerspaces.org/w/api.php"
+hackerspaces_org_api_url = "https://wiki.hackerspaces.org/w/api.php"
 
 
 class Lab(object):
-	"""Represents a Lab as it is described on hackerspaces.org."""
-	
-	def __init__(self):
-		self.logo=""
-		self.country=""
-		self.state=""
-		self.city=""
-		self.founding=""
-		self.coordinates=""
-		self.lat=""
-		self.long=""
-		self.membercount=""
-		self.fee=""
-		self.size=""
-		self.status=""
-		self.site=""
-		self.wiki=""
-		self.irc=""
-		self.jabber=""
-		self.phone=""
-		self.youtube=""
-		self.eventbrite=""
-		self.facebook=""
-		self.ustream=""
-		self.flickr=""
-		self.twitter=""
-		self.googleplus=""
-		self.email=""
-		self.maillist=""
-		self.ical=""
-		self.forum=""
-		self.street_address=""
-		self.postalcode=""
-		self.region=""
-		self.post_office_box=""
-		self.text=""
-		self.equipment=[]
-		
+
+    """Represents a Lab as it is described on hackerspaces.org."""
+
+    def __init__(self):
+        self.logo = ""
+        self.country = ""
+        self.state = ""
+        self.city = ""
+        self.founding = ""
+        self.coordinates = ""
+        self.lat = ""
+        self.long = ""
+        self.membercount = ""
+        self.fee = ""
+        self.size = ""
+        self.status = ""
+        self.site = ""
+        self.wiki = ""
+        self.irc = ""
+        self.jabber = ""
+        self.phone = ""
+        self.youtube = ""
+        self.eventbrite = ""
+        self.facebook = ""
+        self.ustream = ""
+        self.flickr = ""
+        self.twitter = ""
+        self.googleplus = ""
+        self.email = ""
+        self.maillist = ""
+        self.ical = ""
+        self.forum = ""
+        self.street_address = ""
+        self.postalcode = ""
+        self.region = ""
+        self.post_office_box = ""
+        self.text = ""
+        self.equipment = []
+
 
 def get_single_lab(lab_slug, data_format):
-	"""Gets data from a single lab from hackerspaces.org."""
-	wiki = MediaWiki(hackerspaces_org_api_url)
-	wiki_response = wiki.call({'action': 'query', 'titles':lab_slug, 'prop': 'revisions', 'rvprop': 'content'})
-	
-	# If we don't know the pageid...
-	for i in wiki_response["query"]["pages"]:
-		content = wiki_response["query"]["pages"][i]["revisions"][0]["*"]
-	
-	# Transform the data into a Lab object
-	current_lab = Lab()
-	
-	equipment_list = []
-	
-	# Parse the Mediawiki code	
-	wikicode = mwparserfromhell.parse(content)
-	for k in wikicode.filter_templates():
-		element_name = unicode(k.name)
-		if "Hackerspace" in element_name:
-			for j in k.params:
-				if unicode(j.name) == "logo":
-					current_lab.logo = unicode(j.value)
-				if unicode(j.name) == "country":
-					current_lab.country = unicode(j.value)
-				if unicode(j.name) == "state":
-					current_lab.state = unicode(j.value)
-				if unicode(j.name) == "city":
-					current_lab.city = unicode(j.value)
-				if unicode(j.name) == "founding":
-					current_lab.city = unicode(j.value)
-				if unicode(j.name) == "coordinate":
-					value = unicode(j.value)
-					current_lab.coordinates = value
-					latlong = []
-					if ", " in value:
-						latlong = value.rstrip(", ").split(", ")
-					elif " , " in value:
-						latlong = value.rstrip(" , ").split(" , ")
-					else:
-						latlong = ["",""]
-					current_lab.lat = latlong[0]
-					current_lab.long = latlong[1]
-				if unicode(j.name) == "membercount":
-					current_lab.membercount = unicode(j.value)
-				if unicode(j.name) == "fee":
-					current_lab.fee = unicode(j.value)
-				if unicode(j.name) == "size":
-					current_lab.size = unicode(j.value)
-				if unicode(j.name) == "status":
-					current_lab.status = unicode(j.value)
-				if unicode(j.name) == "site":
-					current_lab.site = unicode(j.value)
-				if unicode(j.name) == "wiki":
-					current_lab.wiki = unicode(j.value)
-				if unicode(j.name) == "irc":
-					current_lab.irc = unicode(j.value)
-				if unicode(j.name) == "jabber":
-					current_lab.jabber = unicode(j.value)
-				if unicode(j.name) == "phone":
-					current_lab.phone = unicode(j.value)
-				if unicode(j.name) == "youtube":
-					current_lab.youtube = unicode(j.value)
-				if unicode(j.name) == "eventbrite":
-					current_lab.eventbrite = unicode(j.value)
-				if unicode(j.name) == "facebook":
-					current_lab.facebook = unicode(j.value)
-				if unicode(j.name) == "ustream":
-					current_lab.ustream = unicode(j.value)
-				if unicode(j.name) == "flickr":
-					current_lab.flickr = unicode(j.value)
-				if unicode(j.name) == "twitter":
-					current_lab.twitter = unicode(j.value)
-				if unicode(j.name) == "googleplus":
-					current_lab.googleplus = unicode(j.value)
-				if unicode(j.name) == "email":
-					current_lab.email = unicode(j.value)
-				if unicode(j.name) == "maillist":
-					current_lab.maillist = unicode(j.value)
-				if unicode(j.name) == "ical":
-					current_lab.ical = unicode(j.value)
-				if unicode(j.name) == "forum":
-					current_lab.forum = unicode(j.value)
-				if unicode(j.name) == "street-address":
-					current_lab.street_address = unicode(j.value)
-				if unicode(j.name) == "postalcode":
-					current_lab.postalcode = unicode(j.value)
-				if unicode(j.name) == "region":
-					current_lab.region = unicode(j.value)
-				if unicode(j.name) == "post-office-box":
-					current_lab.post_office_box = unicode(j.value)
-		elif "Equipment" in element_name:
-			for j in k.params:
-				equipment_list.append(j.replace("equipment=", ""))
-	
-	current_lab.equipment = equipment_list
-			
-	# Load the free text
-	freetext = ""
-	for k in wikicode._nodes:
-		try:
-			test_value = k.name
-		except AttributeError:
-			freetext += unicode(k)
-	current_lab.text = freetext
-	
-	if data_format == "dict":
-		return current_lab.__dict__
-	elif data_format == "object":
-		return current_lab
+    """Gets data from a single lab from hackerspaces.org."""
+    wiki = MediaWiki(hackerspaces_org_api_url)
+    wiki_response = wiki.call(
+        {'action': 'query',
+         'titles': lab_slug,
+         'prop': 'revisions',
+         'rvprop': 'content'})
+
+    # If we don't know the pageid...
+    for i in wiki_response["query"]["pages"]:
+        content = wiki_response["query"]["pages"][i]["revisions"][0]["*"]
+
+    # Transform the data into a Lab object
+    current_lab = Lab()
+
+    equipment_list = []
+
+    # Parse the Mediawiki code
+    wikicode = mwparserfromhell.parse(content)
+    for k in wikicode.filter_templates():
+        element_name = unicode(k.name)
+        if "Hackerspace" in element_name:
+            for j in k.params:
+                if unicode(j.name) == "logo":
+                    current_lab.logo = unicode(j.value)
+                if unicode(j.name) == "country":
+                    current_lab.country = unicode(j.value)
+                if unicode(j.name) == "state":
+                    current_lab.state = unicode(j.value)
+                if unicode(j.name) == "city":
+                    current_lab.city = unicode(j.value)
+                if unicode(j.name) == "founding":
+                    current_lab.city = unicode(j.value)
+                if unicode(j.name) == "coordinate":
+                    value = unicode(j.value)
+                    current_lab.coordinates = value
+                    latlong = []
+                    if ", " in value:
+                        latlong = value.rstrip(", ").split(", ")
+                    elif " , " in value:
+                        latlong = value.rstrip(" , ").split(" , ")
+                    else:
+                        latlong = ["", ""]
+                    current_lab.lat = latlong[0]
+                    current_lab.long = latlong[1]
+                if unicode(j.name) == "membercount":
+                    current_lab.membercount = unicode(j.value)
+                if unicode(j.name) == "fee":
+                    current_lab.fee = unicode(j.value)
+                if unicode(j.name) == "size":
+                    current_lab.size = unicode(j.value)
+                if unicode(j.name) == "status":
+                    current_lab.status = unicode(j.value)
+                if unicode(j.name) == "site":
+                    current_lab.site = unicode(j.value)
+                if unicode(j.name) == "wiki":
+                    current_lab.wiki = unicode(j.value)
+                if unicode(j.name) == "irc":
+                    current_lab.irc = unicode(j.value)
+                if unicode(j.name) == "jabber":
+                    current_lab.jabber = unicode(j.value)
+                if unicode(j.name) == "phone":
+                    current_lab.phone = unicode(j.value)
+                if unicode(j.name) == "youtube":
+                    current_lab.youtube = unicode(j.value)
+                if unicode(j.name) == "eventbrite":
+                    current_lab.eventbrite = unicode(j.value)
+                if unicode(j.name) == "facebook":
+                    current_lab.facebook = unicode(j.value)
+                if unicode(j.name) == "ustream":
+                    current_lab.ustream = unicode(j.value)
+                if unicode(j.name) == "flickr":
+                    current_lab.flickr = unicode(j.value)
+                if unicode(j.name) == "twitter":
+                    current_lab.twitter = unicode(j.value)
+                if unicode(j.name) == "googleplus":
+                    current_lab.googleplus = unicode(j.value)
+                if unicode(j.name) == "email":
+                    current_lab.email = unicode(j.value)
+                if unicode(j.name) == "maillist":
+                    current_lab.maillist = unicode(j.value)
+                if unicode(j.name) == "ical":
+                    current_lab.ical = unicode(j.value)
+                if unicode(j.name) == "forum":
+                    current_lab.forum = unicode(j.value)
+                if unicode(j.name) == "street-address":
+                    current_lab.street_address = unicode(j.value)
+                if unicode(j.name) == "postalcode":
+                    current_lab.postalcode = unicode(j.value)
+                if unicode(j.name) == "region":
+                    current_lab.region = unicode(j.value)
+                if unicode(j.name) == "post-office-box":
+                    current_lab.post_office_box = unicode(j.value)
+        elif "Equipment" in element_name:
+            for j in k.params:
+                equipment_list.append(j.replace("equipment=", ""))
+
+    current_lab.equipment = equipment_list
+
+    # Load the free text
+    freetext = ""
+    for k in wikicode._nodes:
+        try:
+            test_value = k.name
+        except AttributeError:
+            freetext += unicode(k)
+    current_lab.text = freetext
+
+    if data_format == "dict":
+        return current_lab.__dict__
+    elif data_format == "object":
+        return current_lab
 
 
 def get_labs(data_format):
-	"""Gets data from all labs from hackerspaces.org."""
-	
-	labs = []
-	
-	# Get the first page of data
-	wiki = MediaWiki(hackerspaces_org_api_url)
-	wiki_response = wiki.call({'action': 'query', 'list': 'categorymembers', 'cmtitle': 'Category:Hackerspace','cmlimit': '500'})
-	nextpage = wiki_response["query-continue"]["categorymembers"]["cmcontinue"]
-	
-	urls = []
-	for i in wiki_response["query"]["categorymembers"]:
-		urls.append(i["title"].replace(" ", "_"))
-	
-	# Load all the Labs in the first page
-	for i in urls:
-		current_lab = get_single_lab(i,data_format)
-		labs.append(current_lab)
-	
-	# Load all the Labs from the other pages
-	while "query-continue" in wiki_response:
-		wiki = MediaWiki(hackerspaces_org_api_url)
-		wiki_response = wiki.call({'action': 'query', 'list': 'categorymembers', 'cmtitle': 'Category:Hackerspace','cmlimit': '500', "cmcontinue":nextpage})
-		
-		urls = []
-		for i in wiki_response["query"]["categorymembers"]:
-			urls.append(i["title"].replace(" ", "_"))
-		
-		# Load all the Labs
-		for i in urls:
-			current_lab = get_single_lab(i,data_format)
-			labs.append(current_lab)
-		
-		if "query-continue" in wiki_response:
-			nextpage = wiki_response["query-continue"]["categorymembers"]["cmcontinue"]
-		else:
-			break
-	
-	# Transform the list into a dictionary
-	labs_dict = {}
-	for j,k in enumerate(labs):
-		labs_dict[j] = k
-	
-	return labs_dict
+    """Gets data from all labs from hackerspaces.org."""
 
-	
+    labs = []
+
+    # Get the first page of data
+    wiki = MediaWiki(hackerspaces_org_api_url)
+    wiki_response = wiki.call(
+        {'action': 'query',
+         'list': 'categorymembers',
+         'cmtitle': 'Category:Hackerspace',
+         'cmlimit': '500'})
+    nextpage = wiki_response["query-continue"]["categorymembers"]["cmcontinue"]
+
+    urls = []
+    for i in wiki_response["query"]["categorymembers"]:
+        urls.append(i["title"].replace(" ", "_"))
+
+    # Load all the Labs in the first page
+    for i in urls:
+        current_lab = get_single_lab(i, data_format)
+        labs.append(current_lab)
+
+    # Load all the Labs from the other pages
+    while "query-continue" in wiki_response:
+        wiki = MediaWiki(hackerspaces_org_api_url)
+        wiki_response = wiki.call({'action': 'query',
+                                   'list': 'categorymembers',
+                                   'cmtitle': 'Category:Hackerspace',
+                                   'cmlimit': '500',
+                                   "cmcontinue": nextpage})
+
+        urls = []
+        for i in wiki_response["query"]["categorymembers"]:
+            urls.append(i["title"].replace(" ", "_"))
+
+        # Load all the Labs
+        for i in urls:
+            current_lab = get_single_lab(i, data_format)
+            labs.append(current_lab)
+
+        if "query-continue" in wiki_response:
+            nextpage = wiki_response[
+                "query-continue"]["categorymembers"]["cmcontinue"]
+        else:
+            break
+
+    # Transform the list into a dictionary
+    labs_dict = {}
+    for j, k in enumerate(labs):
+        labs_dict[j] = k
+
+    return labs_dict
+
+
 def labs_count():
-	"""Gets the number of current Labs registered on hackerspaces.org."""
-	
-	labs = get_labs(data_format="dict")
-	
-	return len(labs)
-	
+    """Gets the number of current Labs registered on hackerspaces.org."""
+
+    labs = get_labs(data_format="dict")
+
+    return len(labs)
+
 
 if __name__ == "__main__":
-	pass
+    pass
--- makerspaces/makeinitaly_foundation.py
+++ makerspaces/makeinitaly_foundation.py
@@ -9,188 +9,207 @@
 #
 
 from simplemediawiki import MediaWiki
-#Requirement: kitchen
+
 
 makeinitaly__foundation_api_url = "http://makeinitaly.foundation/wiki/api.php"
 
 
 class Lab(object):
-	"""Represents a Lab as it is described on makeinitaly.foundation."""
-	
-	def __init__(self):
-		self.long = ""
-		self.lat = ""
-		self.coordinates = ""
-		self.province = ""
-		self.region = ""
-		self.address = ""
-		self.city = ""
-		self.fablabsio = ""
-		self.website = ""
-		self.facebook = ""
-		self.twitter = ""
-		self.email = ""
-		self.manager = ""
-		self.birthyear = ""
-		self.text_it = ""
-		self.text_en = ""
+
+    """Represents a Lab as it is described on makeinitaly.foundation."""
+
+    def __init__(self):
+        self.long = ""
+        self.lat = ""
+        self.coordinates = ""
+        self.province = ""
+        self.region = ""
+        self.address = ""
+        self.city = ""
+        self.fablabsio = ""
+        self.website = ""
+        self.facebook = ""
+        self.twitter = ""
+        self.email = ""
+        self.manager = ""
+        self.birthyear = ""
+        self.text_it = ""
+        self.text_en = ""
 
 
 def get_lab_text(lab_slug, language):
-	"""Gets text description in English or Italian from a single lab from makeinitaly.foundation."""
-	if language == "English" or language == "english" or language == "EN" or language == "En":
-		language = "en"
-	elif language == "Italian" or language == "italian" or language == "IT" or language == "It" or language == "it":
-		language = "it"
-	else:
-		language = "en" 
-	wiki = MediaWiki(makeinitaly__foundation_api_url)
-	wiki_response = wiki.call({'action': 'query', 'titles':lab_slug+"/"+language, 'prop': 'revisions', 'rvprop': 'content'})
+    """Gets text description in English or Italian from a single lab from makeinitaly.foundation."""
+    if language == "English" or language == "english" or language == "EN" or language == "En":
+        language = "en"
+    elif language == "Italian" or language == "italian" or language == "IT" or language == "It" or language == "it":
+        language = "it"
+    else:
+        language = "en"
+    wiki = MediaWiki(makeinitaly__foundation_api_url)
+    wiki_response = wiki.call(
+        {'action': 'query',
+         'titles': lab_slug + "/" + language,
+         'prop': 'revisions',
+         'rvprop': 'content'})
 
-	# If we don't know the pageid...
-	for i in wiki_response["query"]["pages"]:
-		if "revisions" in wiki_response["query"]["pages"][i]:
-			content = wiki_response["query"]["pages"][i]["revisions"][0]["*"]
-		else:
-			content = ""
-	
-	# Clean the resulting string/list
-	newstr01 = content.replace("}}", "")
-	newstr02 = newstr01.replace("{{", "")
-	result = newstr02.rstrip("\n|").split("\n|")
-	
-	return result[0]
-		
+    # If we don't know the pageid...
+    for i in wiki_response["query"]["pages"]:
+        if "revisions" in wiki_response["query"]["pages"][i]:
+            content = wiki_response["query"]["pages"][i]["revisions"][0]["*"]
+        else:
+            content = ""
+
+    # Clean the resulting string/list
+    newstr01 = content.replace("}}", "")
+    newstr02 = newstr01.replace("{{", "")
+    result = newstr02.rstrip("\n|").split("\n|")
+
+    return result[0]
+
 
 def get_single_lab(lab_slug, data_format):
-	"""Gets data from a single lab from makeinitaly.foundation."""
-	wiki = MediaWiki(makeinitaly__foundation_api_url)
-	wiki_response = wiki.call({'action': 'query', 'titles':lab_slug, 'prop': 'revisions', 'rvprop': 'content'})
-	
-	# If we don't know the pageid...
-	for i in wiki_response["query"]["pages"]:
-		content = wiki_response["query"]["pages"][i]["revisions"][0]["*"]
-	
-	# Clean the resulting string/list
-	newstr01 = content.replace("}}", "")
-	newstr02 = newstr01.replace("{{", "")
-	result = newstr02.rstrip("\n|").split("\n|")
-	result.remove(u'FabLab')
-	
-	# Transform the data into a Lab object
-	current_lab = Lab()
-	
-	# Add existing data
-	for i in result:
-		if "coordinates=" in i:
-			value = i.replace("coordinates=", "")
-			current_lab.coordinates = value
-			latlong = []
-			if ", " in value:
-				latlong = value.rstrip(", ").split(", ")
-			elif " , " in value:
-				latlong = value.rstrip(" , ").split(" , ")
-			else:
-				latlong = ["",""]
-			current_lab.lat = latlong[0]
-			current_lab.long = latlong[1]
-		elif "province=" in i:
-			value = i.replace("province=", "")
-			current_lab.province = value.upper()
-		elif "region=" in i:
-			value = i.replace("region=", "")
-			current_lab.region = value
-		elif "address=" in i:
-			value = i.replace("address=", "")
-			current_lab.address = value
-		elif "city=" in i:
-			value = i.replace("city=", "")
-			current_lab.city = value
-		elif "fablabsio=" in i:
-			value = i.replace("fablabsio=", "")
-			current_lab.fablabsio = value
-		elif "website=" in i:
-			value = i.replace("website=", "")
-			current_lab.website = value
-		elif "facebook=" in i:
-			value = i.replace("facebook=", "")
-			current_lab.facebook = value
-		elif "twitter=" in i:
-			value = i.replace("twitter=", "")
-			current_lab.twitter = value
-		elif "email=" in i:
-			value = i.replace("email=", "")
-			current_lab.email = value
-		elif "manager=" in i:
-			value = i.replace("manager=", "")
-			current_lab.manager = value
-		elif "birthyear=" in i:
-			value = i.replace("birthyear=", "")
-			current_lab.birthyear = value
-		
-	current_lab.text_en = get_lab_text(lab_slug=lab_slug, language="en")
-	current_lab.text_it = get_lab_text(lab_slug=lab_slug, language="it")
+    """Gets data from a single lab from makeinitaly.foundation."""
+    wiki = MediaWiki(makeinitaly__foundation_api_url)
+    wiki_response = wiki.call(
+        {'action': 'query',
+         'titles': lab_slug,
+         'prop': 'revisions',
+         'rvprop': 'content'})
 
-	if data_format == "dict":
-		return current_lab.__dict__
-	elif data_format == "object":
-		return current_lab
-	
+    # If we don't know the pageid...
+    for i in wiki_response["query"]["pages"]:
+        content = wiki_response["query"]["pages"][i]["revisions"][0]["*"]
+
+    # Clean the resulting string/list
+    newstr01 = content.replace("}}", "")
+    newstr02 = newstr01.replace("{{", "")
+    result = newstr02.rstrip("\n|").split("\n|")
+    # result.remove(u'FabLab')
+
+    # Transform the data into a Lab object
+    current_lab = Lab()
+
+    # Add existing data
+    for i in result:
+        if "coordinates=" in i:
+            value = i.replace("coordinates=", "")
+            current_lab.coordinates = value
+            latlong = []
+            if ", " in value:
+                latlong = value.rstrip(", ").split(", ")
+            elif " , " in value:
+                latlong = value.rstrip(" , ").split(" , ")
+            else:
+                latlong = ["", ""]
+            current_lab.lat = latlong[0]
+            current_lab.long = latlong[1]
+        elif "province=" in i:
+            value = i.replace("province=", "")
+            current_lab.province = value.upper()
+        elif "region=" in i:
+            value = i.replace("region=", "")
+            current_lab.region = value
+        elif "address=" in i:
+            value = i.replace("address=", "")
+            current_lab.address = value
+        elif "city=" in i:
+            value = i.replace("city=", "")
+            current_lab.city = value
+        elif "fablabsio=" in i:
+            value = i.replace("fablabsio=", "")
+            current_lab.fablabsio = value
+        elif "website=" in i:
+            value = i.replace("website=", "")
+            current_lab.website = value
+        elif "facebook=" in i:
+            value = i.replace("facebook=", "")
+            current_lab.facebook = value
+        elif "twitter=" in i:
+            value = i.replace("twitter=", "")
+            current_lab.twitter = value
+        elif "email=" in i:
+            value = i.replace("email=", "")
+            current_lab.email = value
+        elif "manager=" in i:
+            value = i.replace("manager=", "")
+            current_lab.manager = value
+        elif "birthyear=" in i:
+            value = i.replace("birthyear=", "")
+            current_lab.birthyear = value
+
+    current_lab.text_en = get_lab_text(lab_slug=lab_slug, language="en")
+    current_lab.text_it = get_lab_text(lab_slug=lab_slug, language="it")
+
+    if data_format == "dict":
+        return current_lab.__dict__
+    elif data_format == "object":
+        return current_lab
+
 
 def get_labs(data_format):
-	"""Gets data from all labs from hackerspaces.org."""
-	
-	labs = []
-	
-	# Get the first page of data
-	wiki = MediaWiki(makeinitaly__foundation_api_url)
-	wiki_response = wiki.call({'action': 'query', 'list': 'categorymembers', 'cmtitle': 'Category:Italian_FabLabs','cmlimit': '500'})
-	if "query-continue" in wiki_response:
-		nextpage = wiki_response["query-continue"]["categorymembers"]["cmcontinue"]
-	
-	urls = []
-	for i in wiki_response["query"]["categorymembers"]:
-		urls.append(i["title"].replace(" ", "_"))
-	
-	# Load all the Labs in the first page
-	for i in urls:
-		current_lab = get_single_lab(i,data_format)
-		labs.append(current_lab)
-	
-	# Load all the Labs from the other pages
-	while "query-continue" in wiki_response:
-		wiki = MediaWiki(makeinitaly__foundation_api_url)
-		wiki_response = wiki.call({'action': 'query', 'list': 'categorymembers', 'cmtitle': 'Category:Hackerspace','cmlimit': '500', "cmcontinue":nextpage})
-		
-		urls = []
-		for i in wiki_response["query"]["categorymembers"]:
-			urls.append(i["title"].replace(" ", "_"))
-		
-		# Load all the Labs
-		for i in urls:
-			current_lab = get_single_lab(i,data_format)
-			labs.append(current_lab)
-		
-		if "query-continue" in wiki_response:
-			nextpage = wiki_response["query-continue"]["categorymembers"]["cmcontinue"]
-		else:
-			break
-	
-	# Transform the list into a dictionary
-	labs_dict = {}
-	for j,k in enumerate(labs):
-		labs_dict[j] = k
-	
-	return labs_dict
+    """Gets data from all labs from hackerspaces.org."""
 
-	
+    labs = []
+
+    # Get the first page of data
+    wiki = MediaWiki(makeinitaly__foundation_api_url)
+    wiki_response = wiki.call(
+        {'action': 'query',
+         'list': 'categorymembers',
+         'cmtitle': 'Category:Italian_FabLabs',
+         'cmlimit': '500'})
+    if "query-continue" in wiki_response:
+        nextpage = wiki_response[
+            "query-continue"]["categorymembers"]["cmcontinue"]
+
+    urls = []
+    for i in wiki_response["query"]["categorymembers"]:
+        urls.append(i["title"].replace(" ", "_"))
+
+    # Load all the Labs in the first page
+    for i in urls:
+        current_lab = get_single_lab(i, data_format)
+        labs.append(current_lab)
+
+    # Load all the Labs from the other pages
+    while "query-continue" in wiki_response:
+        wiki = MediaWiki(makeinitaly__foundation_api_url)
+        wiki_response = wiki.call({'action': 'query',
+                                   'list': 'categorymembers',
+                                   'cmtitle': 'Category:Hackerspace',
+                                   'cmlimit': '500',
+                                   "cmcontinue": nextpage})
+
+        urls = []
+        for i in wiki_response["query"]["categorymembers"]:
+            urls.append(i["title"].replace(" ", "_"))
+
+        # Load all the Labs
+        for i in urls:
+            current_lab = get_single_lab(i, data_format)
+            labs.append(current_lab)
+
+        if "query-continue" in wiki_response:
+            nextpage = wiki_response[
+                "query-continue"]["categorymembers"]["cmcontinue"]
+        else:
+            break
+
+    # Transform the list into a dictionary
+    labs_dict = {}
+    for j, k in enumerate(labs):
+        labs_dict[j] = k
+
+    return labs_dict
+
+
 def labs_count():
-	"""Gets the number of current Labs registered on makeinitaly.foundation."""
-	
-	labs = get_labs(data_format="dict")
-	
-	return len(labs)
-	
+    """Gets the number of current Labs registered on makeinitaly.foundation."""
+
+    labs = get_labs(data_format="dict")
+
+    return len(labs)
+
 
 if __name__ == "__main__":
-	pass
+    pass
