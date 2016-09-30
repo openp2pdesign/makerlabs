b4753e85183576ea8947e847273365c873e1c497
Massimo Menichinelli <info@openp2pdesign.org>
2016-09-30 14:29:02 +0200

    Add projects to fablabs_io

--- makerlabs/fablabs_io.py
+++ makerlabs/fablabs_io.py
@@ -44,6 +44,38 @@ class FabLab(object):
         self.url = ""
 
 
+class Project(object):
+    """Represents a project as it is described on fablabs.io."""
+
+    def __init__(self):
+        self.id = ""
+        self.title = ""
+        self.description = ""
+        self.github = ""
+        self.web = ""
+        self.dropbox = ""
+        self.bitbucket = ""
+        self.lab_id = ""
+        self.owner_id = ""
+        self.created_at = ""
+        self.updated_at = ""
+        self.vimeo = ""
+        self.flickr = ""
+        self.youtube = ""
+        self.drive = ""
+        self.twitter = ""
+        self.facebook = ""
+        self.googleplus = ""
+        self.instagram = ""
+        self.status = ""
+        self.version = ""
+        self.faq = ""
+        self.scope = ""
+        self.community = ""
+        self.lookingfor = ""
+        self.cover = ""
+
+
 def data_from_fablabs_io(endpoint):
     """Gets data from fablabs.io."""
 
@@ -53,7 +85,7 @@ def data_from_fablabs_io(endpoint):
 
 
 def get_labs(format):
-    """Gets FabLab data from fablabs.io."""
+    """Gets Fab Lab data from fablabs.io."""
 
     fablabs_json = data_from_fablabs_io(fablabs_io_labs_api_url_v0)
     fablabs = {}
@@ -90,6 +122,8 @@ def get_labs(format):
         output = {}
         for j in fablabs:
             output[j] = fablabs[j].__dict__
+    elif format.lower() == "geojson" or format.lower() == "geo":
+        pass
     elif format.lower() == "object" or format.lower() == "obj":
         output = fablabs
     else:
@@ -104,6 +138,67 @@ def labs_count():
     fablabs = data_from_fablabs_io(fablabs_io_labs_api_url_v0)
 
     return len(fablabs["labs"])
+
+
+def get_projects(format):
+    """Gets projects data from fablabs.io."""
+
+    projects_json = data_from_fablabs_io(fablabs_io_projects_api_url_v0)
+    projects = {}
+    project_url = "https://www.fablabs.io/projects/"
+
+    # Load all the FabLabs
+    for i in projects_json["projects"]:
+        i = i["projects"]
+        current_project = Project()
+        current_project.id = i["id"]
+        current_project.title = i["title"]
+        current_project.description = i["description"]
+        current_project.github = i["github"]
+        current_project.web = i["web"]
+        current_project.dropbox = i["dropbox"]
+        current_project.bitbucket = i["bitbucket"]
+        current_project.lab_id = i["lab_id"]
+        current_project.owner_id = i["owner_id"]
+        current_project.created_at = i["created_at"]
+        current_project.updated_at = i["updated_at"]
+        current_project.vimeo = i["vimeo"]
+        current_project.flickr = i["flickr"]
+        current_project.youtube = i["youtube"]
+        current_project.drive = i["drive"]
+        current_project.twitter = i["twitter"]
+        current_project.facebook = i["facebook"]
+        current_project.googleplus = i["googleplus"]
+        current_project.instagram = i["instagram"]
+        current_project.status = i["status"]
+        current_project.version = i["version"]
+        current_project.faq = i["faq"]
+        current_project.scope = i["scope"]
+        current_project.community = i["community"]
+        current_project.lookingfor = i["lookingfor"]
+        current_project.cover = i["cover"]
+        projects[project_url + str(current_project.id)] = current_project
+
+    if format.lower() == "dict" or format.lower() == "json":
+        output = {}
+        for j in projects:
+            output[j] = projects[j].__dict__
+    elif format.lower() == "geojson" or format.lower() == "geo":
+        pass
+    elif format.lower() == "object" or format.lower() == "obj":
+        output = projects
+    else:
+        output = projects
+
+    return output
+
+
+def projects_count():
+    """Gets the number of current projects submitted on fablabs.io."""
+
+    projects = data_from_fablabs_io(fablabs_io_projects_api_url_v0)
+
+    return len(projects["projects"])
 
 
 if __name__ == "__main__":
