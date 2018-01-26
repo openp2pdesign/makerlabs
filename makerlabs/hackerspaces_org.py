c7fab796d712716edab695683e36ffc253396ec3
Massimo Menichinelli <info@openp2pdesign.org>
2018-01-26 14:09:28 +0100

    fixes in hackerspaces_org.py

--- makerlabs/hackerspaces_org.py
+++ makerlabs/hackerspaces_org.py
@@ -15,6 +15,7 @@ import json
 from simplemediawiki import MediaWiki
 import mwparserfromhell
 import pandas as pd
+from incf.countryutils import transformations
 
 hackerspaces_org_api_url = "https://wiki.hackerspaces.org/w/api.php"
 
@@ -53,21 +54,27 @@ def get_single_lab(lab_slug):
         if "Hackerspace" in element_name:
             for j in k.params:
                 current_lab.name = lab_slug
+                j_value = unicode(j.value)
+                j_name = unicode(j.name)
                 # Remove new line in content
-                if j.value[-2:] == "\n" or j.value[:2] == "\n":
-                    j.value = j.value.replace('\n', '')
+                if j_value[-1:] == "\n" or j_value[:1] == "\n":
+                    j_value = j_value.replace('\n', '')
                 if unicode(j.name) == "logo":
-                    current_lab.logo = unicode(j.value)
-                if unicode(j.name) == "country":
-                    current_lab.country = unicode(j.value)
-                if unicode(j.name) == "state":
-                    current_lab.state = unicode(j.value)
-                if unicode(j.name) == "city":
-                    current_lab.city = unicode(j.value)
-                if unicode(j.name) == "founding":
-                    current_lab.city = unicode(j.value)
-                if unicode(j.name) == "coordinate":
-                    value = unicode(j.value)
+                    current_lab.logo = j_value
+                if j_name == "country":
+                    current_lab.country = j_value
+                    try:
+                        current_lab.continent = transformations.cn_to_ctn(current_lab.country)
+                    except Exception as e:
+                        current_lab.continent = None
+                if j_name == "state":
+                    current_lab.state = j_value
+                if j_name == "city":
+                    current_lab.city = j_value
+                if j_name == "founding":
+                    current_lab.city = j_value
+                if j_name == "coordinate":
+                    value = j_value
                     current_lab.coordinates = value
                     latlong = []
                     if ", " in value:
@@ -78,54 +85,54 @@ def get_single_lab(lab_slug):
                         latlong = ["", ""]
                     current_lab.latitude = latlong[0]
                     current_lab.longitude = latlong[1]
-                if unicode(j.name) == "membercount":
-                    current_lab.membercount = unicode(j.value)
-                if unicode(j.name) == "fee":
-                    current_lab.fee = unicode(j.value)
-                if unicode(j.name) == "size":
-                    current_lab.size = unicode(j.value)
-                if unicode(j.name) == "status":
-                    current_lab.status = unicode(j.value)
-                if unicode(j.name) == "site":
-                    current_lab.site = unicode(j.value)
-                if unicode(j.name) == "wiki":
-                    current_lab.wiki = unicode(j.value)
-                if unicode(j.name) == "irc":
-                    current_lab.irc = unicode(j.value)
-                if unicode(j.name) == "jabber":
-                    current_lab.jabber = unicode(j.value)
-                if unicode(j.name) == "phone":
-                    current_lab.phone = unicode(j.value)
-                if unicode(j.name) == "youtube":
-                    current_lab.youtube = unicode(j.value)
-                if unicode(j.name) == "eventbrite":
-                    current_lab.eventbrite = unicode(j.value)
-                if unicode(j.name) == "facebook":
-                    current_lab.facebook = unicode(j.value)
-                if unicode(j.name) == "ustream":
-                    current_lab.ustream = unicode(j.value)
-                if unicode(j.name) == "flickr":
-                    current_lab.flickr = unicode(j.value)
-                if unicode(j.name) == "twitter":
-                    current_lab.twitter = unicode(j.value)
-                if unicode(j.name) == "googleplus":
-                    current_lab.googleplus = unicode(j.value)
-                if unicode(j.name) == "email":
-                    current_lab.email = unicode(j.value)
-                if unicode(j.name) == "maillist":
-                    current_lab.maillist = unicode(j.value)
-                if unicode(j.name) == "ical":
-                    current_lab.ical = unicode(j.value)
-                if unicode(j.name) == "forum":
-                    current_lab.forum = unicode(j.value)
-                if unicode(j.name) == "street-address":
-                    current_lab.street_address = unicode(j.value)
-                if unicode(j.name) == "postalcode":
-                    current_lab.postalcode = unicode(j.value)
-                if unicode(j.name) == "region":
-                    current_lab.region = unicode(j.value)
-                if unicode(j.name) == "post-office-box":
-                    current_lab.post_office_box = unicode(j.value)
+                if j_name == "membercount":
+                    current_lab.membercount = j_value
+                if j_name == "fee":
+                    current_lab.fee = j_value
+                if j_name == "size":
+                    current_lab.size = j_value
+                if j_name == "status":
+                    current_lab.status = j_value
+                if j_name == "site":
+                    current_lab.site = j_value
+                if j_name == "wiki":
+                    current_lab.wiki = j_value
+                if j_name == "irc":
+                    current_lab.irc = j_value
+                if j_name == "jabber":
+                    current_lab.jabber = j_value
+                if j_name == "phone":
+                    current_lab.phone = j_value
+                if j_name == "youtube":
+                    current_lab.youtube = j_value
+                if j_name == "eventbrite":
+                    current_lab.eventbrite = j_value
+                if j_name == "facebook":
+                    current_lab.facebook = j_value
+                if j_name == "ustream":
+                    current_lab.ustream = j_value
+                if j_name == "flickr":
+                    current_lab.flickr = j_value
+                if j_name == "twitter":
+                    current_lab.twitter = j_value
+                if j_name == "googleplus":
+                    current_lab.googleplus = j_value
+                if j_name == "email":
+                    current_lab.email = j_value
+                if j_name == "maillist":
+                    current_lab.maillist = j_value
+                if j_name == "ical":
+                    current_lab.ical = j_value
+                if j_name == "forum":
+                    current_lab.forum = j_value
+                if j_name == "street-address":
+                    current_lab.street_address = j_value
+                if j_name == "postalcode":
+                    current_lab.postalcode = j_value
+                if j_name == "region":
+                    current_lab.region = j_value
+                if j_name == "post-office-box":
+                    current_lab.post_office_box = j_value
         elif "Equipment" in element_name:
             for j in k.params:
                 equipment_list.append(j.replace("equipment=", ""))
