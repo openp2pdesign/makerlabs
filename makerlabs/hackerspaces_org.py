6ab831b55336502466acbc13218fb620399d7e49
Massimo Menichinelli <info@openp2pdesign.org>
2022-05-01 21:35:57 +0200

    Update hackerspaces_org.py to be more similar to other labs

--- makerlabs/hackerspaces_org.py
+++ makerlabs/hackerspaces_org.py
@@ -36,7 +36,7 @@ def data_from_hackerspaces_org():
 
     hackerspaces = []
     req = requests.Session()
-    # Get first 500 labs
+    # Get first 500 labs names / page titles
     params = {
         "action": "query",
         "cmtitle": "Category:Hackerspace",
@@ -47,7 +47,7 @@ def data_from_hackerspaces_org():
     request_data = req.get(url=API_endpoint, params=params).json()
     for page in request_data['query']['categorymembers']:
         hackerspaces.append(page['title'])
-    # Get next labs
+    # Get all next labs names / page titles
     while "continue" in request_data:
         params = {
             "action": "query",
@@ -62,172 +62,31 @@ def data_from_hackerspaces_org():
         for page in request_data['query']['categorymembers']:
             hackerspaces.append(page['title'])
 
-
     return hackerspaces
 
 
-def get_single_lab(lab_slug, open_cage_api_key):
-    """Gets data from a single lab from hackerspaces.org."""
-
-    # API connection setup
-    request_session = requests.Session()
-
-    # Get the first page of data
-    params = {
-        "action": "parse",
-        "page": lab_slug,
-        "prop": "text",
-        "formatversion": "2",
-        "format": "json"
-    }
-    wiki_response = request_session.get(url=API_endpoint, params=params).json()
-    content = BeautifulSoup(wiki_response['parse']['text'], 'html.parser')
-
-    # Transform the data into a Lab object
-    current_lab = Hackerspace()
-
-    # Parse the Mediawiki code
-    current_lab.name = wiki_response["parse"]["title"]
-    current_lab.id = wiki_response["parse"]["pageid"]
-    current_lab.slug = lab_slug
-    # Coordinates from the map marker
-    for div in content.find_all("div", {"class": "mapdata"}):
-        mapjavascript = json.loads(div.text)["locations"]
-        current_lab.latitude = mapjavascript[0]['lat']
-        current_lab.longitude = mapjavascript[0]['lon']
-
-    # Parse the side table
-    # th and td<b> are mixed...
-    for table in content.find_all('table'):
-        # th
-        for th in table.find_all('th'):
-            #if th.text.strip() == "Status":
-            #    current_lab.membercount = th.find_next('td').text.strip()
-            if th.text.strip() == "Country":
-                current_lab.country = th.find_next('td').text.strip()
-            if th.text.strip() == "State or District":
-                current_lab.state = th.find_next('td').text.strip()
-            if th.text.strip() == "City":
-                current_lab.city = th.find_next('td').text.strip()
-            if th.text.strip() == "Date of founding":
-                current_lab.created_at = th.find_next('td').text.strip()
-            if th.text.strip() == "Website":
-                current_lab.url = th.find_next('td').text.strip()
-            if th.text.strip() == "Facebook":
-                current_lab.membercount = th.find_next('td').text.strip()
-            if th.text.strip() == "Snail mail":
-                current_lab.membercount = th.find_next('td').text.strip()
-            if th.text.strip() == "Number of members":
-                current_lab.membercount = th.find_next('td').text.strip()
-            if th.text.strip() == "Membership fee":
-                current_lab.membercount = th.find_next('td').text.strip()
-        # td
-        for td in table.find_all('td'):
-            if td.text.strip() in keywords:
-                print("B",td,'---', td.find_next('td').text.strip())
-            if td.text.strip() == "Status":
-                current_lab.membercount = td.find_next('td').text.strip()
-            if td.text.strip() == "Country":
-                current_lab.membercount = td.find_next('td').text.strip()
-            if td.text.strip() == "State or District":
-                current_lab.membercount = td.find_next('td').text.strip()
-            if td.text.strip() == "City":
-                current_lab.membercount = td.find_next('td').text.strip()
-            if td.text.strip() == "Date of founding":
-                current_lab.membercount = td.find_next('td').text.strip()
-            if td.text.strip() == "Website":
-                current_lab.membercount = td.find_next('td').text.strip()
-            if td.text.strip() == "Facebook":
-                current_lab.membercount = td.find_next('td').text.strip()
-            if td.text.strip() == "Snail mail":
-                current_lab.membercount = td.find_next('td').text.strip()
-            if td.text.strip() == "Number of members":
-                current_lab.membercount = td.find_next('td').text.strip()
-            if td.text.strip() == "Membership fee":
-                current_lab.membercount = td.find_next('td').text.strip()
-
-    # TODO
-    # Find Facebook and Twitter links, add also the other ones
-    current_lab.links = {"facebook": "", "twitter": ""}
-    for link in i["links"]:
-        if "facebook" in link["url"]:
-            current_lab.links["facebook"] = link["url"]
-        elif "twitter" in link["url"]:
-            current_lab.links["twitter"] = link["url"]
-        else:
-            current_lab.links[link["id"]] = link["url"]
-
-    # TODO
-        if j_name == "coordinate":
-            # Get the full address with the coordinates
-            address = get_location(query=j_value, format="reverse", api_key=open_cage_api_key)
-            current_lab.city = address["city"]
-            current_lab.county = address["county"]
-            current_lab.state = address["state"]
-            current_lab.postal_code = address["postal_code"]
-            current_lab.address_1 = address["address_1"]
-            current_lab.country = address["country"]
-            current_lab.country_code = address["country_code"]
-            current_lab.continent = address["continent"]
-            current_lab.latitude = address["latitude"]
-            current_lab.longitude = address["longitude"]
-
-
-    return current_lab
-
-
 def get_labs(format):
-    """Gets data from all labs from hackerspaces.org."""
+    """Gets data from all hackerspaces from hackerspaces.org."""
 
-    labs = []
+    labs_titles = data_from_hackerspaces_org()
+    labs = {}
 
-    # API connection setup
-    request_session = requests.Session()
-
-    # Get the first page of data
-    params = {
-        "action": "query",
-        "cmtitle": "Category:Hackerspace",
-        "cmlimit": "500",
-        "list": "categorymembers",
-        "format": "json"
-    }
-    wiki_response = request_session.get(url=API_endpoint, params=params).json()
-
-    urls = []
-    for i in wiki_response["query"]["categorymembers"]:
-        urls.append(i["title"].replace(" ", "_"))
-
-    # Load all the Labs in the first page
-    for i in urls:
-        current_lab = get_single_lab(i, open_cage_api_key)
-        labs.append(current_lab)
-
-    # Load all the Labs from the other pages
-    while "continue" in wiki_response:
+    # Load all the Hackerspaces
+    for i in labs_titles:
+        current_lab = Hackerspace()
+        req = requests.Session()
         params = {
-            "action": "query",
-            "cmtitle": "Category:Hackerspace",
-            "cmlimit": "500",
-            "cmcontinue": wiki_response['continue']['cmcontinue'],
-            "list": "categorymembers",
+            "action": "parse",
+            "page": i,
+            "prop": "text",
+            "formatversion": "2",
             "format": "json"
         }
-        wiki_response = request_session.get(url=API_endpoint, params=params).json()
-
-        urls = []
-        for i in wiki_response["query"]["categorymembers"]:
-            urls.append(i["title"].replace(" ", "_"))
-
-        # Load all the Labs
-        for i in urls:
-            current_lab = get_single_lab(i)
-            labs.append(current_lab)
-
-    # Transform the list into a dictionary
-    labs_dict = {}
-    for j, k in enumerate(labs):
-        labs_dict[j] = k.__dict__
+        request_data = req.get(url=API_endpoint, params=params).json()
+        print(request_data)
+        exit()
+        for page in request_data['query']['categorymembers']:
+            hackerspaces.append(page['title'])
 
     # Return formatted data
     data = format_labs_data(format=format, labs=labs)
@@ -238,12 +97,12 @@ def get_labs(format):
 def labs_count():
     """Gets the number of current Hackerspaces registered on hackerspaces.org."""
 
-    labs = get_labs()
+    labs = data_from_hackerspaces_org()
 
     return len(labs)
 
 
 if __name__ == "__main__":
     #pass
-    a = data_from_hackerspaces_org()
+    a = get_labs(format="dict")
     print(a)
